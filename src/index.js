ac24cc86732e515dbae11134a7504a0c2ab4cae2
rydvone <rydvone@gmail.com>
2022-01-07 23:43:03 +0300

    init: start work with task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let a = '';
+  n = Math.abs(n);
+    function rev(n) {
+      if (n >= 1) {
+        a = a + (n % 10);
+        rev(Math.floor(n / 10));
+      }
+      return a;
+    }    
+  return n = +(rev(n));  
 }
